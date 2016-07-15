0f9441257a4e3c00409b80cb284462d2d234bb98
BoloBolo <gbajordan@gmail.com>
2016-07-14 23:15:49 -0300

    corringindo erro

--- exemplo.py
+++ exemplo.py
@@ -4,7 +4,7 @@ import Lab01RI
 
 dicionario= {}
 dicionario= Lab01RI.arrumaDocumento("C:\Users\jordan\Documents\ptwiki-v2.txt", "C:\Users\jordan\Desktop\stopwords.txt")
-Lab01RI.colocArquivo(dicionario)
+Lab01RI.colocArquivo(dicionario, "C:\Users\jordan\Documents")
 
 print(Lab01RI.intersect("bíblicos", "nomes", dicionario))
 
